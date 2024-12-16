1a1fc907574e4f65cfb270d37174bd7b071b04b1
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-12-16 16:57:10 +0300

    Navbar responsivity

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,25 +1,38 @@
 import React from 'react'
+import { useRef } from 'react';
 import { HashLink as Link } from "react-router-hash-link";
 import './Navbar.css'
+import { FaBars, FaTimes } from "react-icons/fa";
 import logo from '../Images/logo.png'
 
 function NavBar() {
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
+
   return (
-    <div className='NavBar'>
-        <div className='navbar-container'>
-            <div className='logo-container'>
-                <img src={logo} alt="Debz" className='logo'/>
-            </div>
-            <div className='navigation'>
-                <Link to="#Heropage">Home</Link>
-                <Link to="#About">About</Link>
-                <Link to="#Experiences">Experiences</Link>
-                <Link to="#Expertise">Expertise</Link>
-                <Link to="#Certifications">Certifications</Link>
-                <Link to="#Testimonials">Testimonials</Link>
-            </div>
+    <header className='NavBar'>
+      <div className='navbar-container'>
+        <div className='logo-container'>
+          <img src={logo} alt="Debz" className='logo'/>
         </div>
-    </div>
+        <nav className='navigation' ref={navRef}>
+          <Link to="#Heropage">Home</Link>
+          <Link to="#About">About</Link>
+          <Link to="#Experiences">Experiences</Link>
+          <Link to="#Expertise">Expertise</Link>
+          <Link to="#Certifications">Certifications</Link>
+          <Link to="#Testimonials">Testimonials</Link>
+          <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button className='nav-btn' onClick={showNavbar}>
+          <FaBars/>
+        </button>
+      </div>
+    </header>
   )
 }
 
